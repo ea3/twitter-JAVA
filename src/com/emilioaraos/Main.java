f0ca23e86b511359ccaa1f4e336e95892d37db33
Emilio Araos <ea3dev@gmail.com>
2019-11-14 13:20:04 -0700

    setting up repo

---
+++ src/com/emilioaraos/Main.java
@@ -0,0 +1,16 @@
+package com.emilioaraos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        X(1, Integer.MAX_VALUE +1);
+    }
+
+    public static void X (int v1, int v2){
+        System.out.println("1");
+    }
+    public static void X (int... v){
+        System.out.println("2");
+    }
+
+}
